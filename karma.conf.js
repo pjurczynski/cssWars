f7e270968a371b1d3f0a303176666e556abad9e4
Peter Jurczyński <pjurczynski@gmail.com>
2013-10-28 12:20:29 +0100

    revert browser settings

--- karma.conf.js
+++ karma.conf.js
@@ -45,8 +45,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    // browsers: ['Chrome'], 
-    // it mounts the second browser, why?
+    browsers: ['PhantomJS'], 
 
 
     // Continuous Integration mode
